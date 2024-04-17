010430e98bcffea2c7520c7ae0bb2a120716f01b
mehdizebhi <mehdizebhi@yahoo.com>
2024-04-17 09:20:05 +0330

    update movie controller endpoints to return dto

--- src/main/java/dev/mehdizebhi/springdatameilisearchdemo/controller/MovieController.java
+++ src/main/java/dev/mehdizebhi/springdatameilisearchdemo/controller/MovieController.java
@@ -1,11 +1,12 @@
 package dev.mehdizebhi.springdatameilisearchdemo.controller;
 
+import dev.mehdizebhi.springdatameilisearchdemo.dto.ResponseModel;
 import dev.mehdizebhi.springdatameilisearchdemo.model.Movie;
 import dev.mehdizebhi.springdatameilisearchdemo.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,21 +15,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/movies")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class MovieController {
 
     private final MovieRepository movieRepository;
 
     @GetMapping("")
-    public Page<Movie> getMovies(
+    public ResponseEntity<ResponseModel<List<Movie>>> getMovies(
             @PageableDefault Pageable pageable,
             @RequestParam("q") String query
     ) {
-        return movieRepository.findByQuery(query, pageable);
+        var movies = movieRepository.findByQuery(query, pageable);
+        return ResponseEntity
+                .ok(
+                        ResponseModel.builder()
+                        .content(movies.getContent())
+                        .pageMetadata(movies).build()
+                );
     }
 
     @PostMapping("/sort")
-    public ResponseEntity<String> setSorts(@RequestParam("p") String[] properties) {
+    public ResponseEntity<Void> setSorts(@RequestParam("p") String[] properties) {
         movieRepository.updateSortableAttributes(properties);
-        return ResponseEntity.ok("Added");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
